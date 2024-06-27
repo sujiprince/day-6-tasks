61cc6460389a9942de934edd5a634d67172b4c32
sujith <sujikn73@gmail.com>
2024-06-28 01:19:50 +0400

    programm movie

--- oops.js
+++ oops.js
@@ -16,9 +16,9 @@ class Movie {
     }
   }
   
-  const singam1 = new Movie("SPD1", "EROS");
-  const singam2 = new Movie("SPD2", "EROS");
-  const singam3 = new Movie("SPD3", "EROS", "PG13");
-  const vikram = new Movie("IR1", "EROS", "R");
+  const singam1 = new Movie("singam1", "EROS");
+  const singam2 = new Movie("singam2", "EROS");
+  const singam3 = new Movie("singam3", "EROS", "PG13");
+  const vikram = new Movie("vikram", "EROS", "R");
   
   console.log(singam1.getPG([singam1, singam2, singam3, vikram]));
